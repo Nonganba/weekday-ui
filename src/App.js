2292e2da2d1677e0783434ef3fb61e179c1def4a
Nonganba <nanganba.th1109@gmail.com>
2024-05-15 18:09:43 +0530

    create filter component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useCallback, useRef, useState } from "react";
 import "./App.css";
 import { useGetJobsQuery } from "./slices/jobApiSlice";
 import JobCard from "./components/JobCard";
+import { Stack } from "@mui/material";
+import Filter from "./components/Filter";
 
 const App = () => {
   const [page, setPage] = useState(1);
@@ -29,6 +31,26 @@ const App = () => {
 
   return (
     <div className="App">
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{
+          overflowY: "auto",
+          position: "sticky",
+          top: 0,
+          zIndex: 2,
+          backgroundColor: "white",
+          paddingTop: "15px",
+          paddingBottom: "15px",
+        }}
+      >
+        <Filter filterName="Roles" />
+        <Filter filterName="Number of Employees" />
+        <Filter filterName="Experience" />
+        <Filter filterName="Remote" />
+        <Filter filterName="Minimum Base Pay Salary" />
+        <Filter filterName="Search Company Name" />
+      </Stack>
       {jobs.length <= 0 && !isFetching ? (
         <div ref={lastJobElementRef}>
           <h1>Oops...No jobs found !!</h1>
---
+++ src/components/Filter.js
@@ -0,0 +1,33 @@
+import { Divider, IconButton, InputBase, Paper } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import React from "react";
+
+const Filter = ({ filterName }) => {
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: "fit-content",
+        minWidth: "200px",
+        border: "1px solid #808080",
+        width: "max-content",
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, mr: 1, fontSize: "15px" }}
+        placeholder={filterName}
+        inputProps={{ "aria-label": filterName }}
+      />
+      <Divider sx={{ height: 28 }} orientation="vertical" />
+      <IconButton color="disabled" sx={{ p: "5px" }} aria-label="directions">
+        <ExpandMoreIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default Filter;
